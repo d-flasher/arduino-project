e40a6a336277fbb7913f5cd2defc4b432a3ddc6c
denis <sdr-u@mail.ru>
2018-01-07 00:37:49 +0500

    проверка 2х серв одновременно

--- servo-test/servo-test.ino
+++ servo-test/servo-test.ino
@@ -1,32 +1,36 @@
 #include <Servo.h>
 
-Servo myservo;
+Servo myservo1;
+Servo myservo2;
 int DELAY;
 int isLoop;
 
 void setup() {
   Serial.begin(9600);
-  myservo.attach(8);
-  DELAY = 5000;
+  myservo1.attach(4);
+  myservo2.attach(8);
+  DELAY = 3000;
 }
 
 void loop() {
   if (isLoop == 1) return;
   Serial.println("loop");
 
-  int MY_SIZE = 9;
-  int myarray[MY_SIZE] = {0, 23, 45, 67, 90, 112, 135, 157, 180};
+  int MY_SIZE = 5;
+  int myarray[MY_SIZE] = {45, 67, 90, 112, 135};
   for (int i = 0; i < MY_SIZE; i++) {
     Serial.println(String(myarray[i]) + " rad");
     Serial.println("");
-    myservo.write(myarray[i]);
+    myservo1.write(myarray[i]);
+    myservo2.write(myarray[i]);
 
     delay(DELAY);
 
     if (i == MY_SIZE - 1) {
       Serial.println("return");
       Serial.println("");
-      myservo.detach();
+      myservo1.detach();
+      myservo2.detach();
     }
   }
 
